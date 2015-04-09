8370e855e17e2bc85aeef858c23aeb7993035c0b
Jonathan Lin & Kyle Giard-Chase <student@epicodus.com>
2015-04-09 11:23:41 -0700

    Add code to sort through words in a sentence and the amount of appearances the word appears in a sentence

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,19 @@
-var wordSort = function() {
-    return 'hello';
+var wordSort = function(input) {
+
+    var split_input = input.split(' ');
+    var new_array = {};
+
+    split_input.forEach(function(word)
+    {
+        if (new_array[word])
+        {
+            new_array[word] += 1;
+        }
+        else
+        {
+            new_array[word] = 1;
+        }
+    });
+    return new_array;
+
 }
--- spec/specs.js
+++ spec/specs.js
@@ -1,5 +1,9 @@
 describe('wordSort' ,function() {
     it("will return one word if user inputs one word", function() {
-        expect(wordSort('hello')).to.equal('hello')
+        expect(wordSort('hello')).to.eql({hello:1})
+    });
+
+    it("will return two unique words if user inputs two unique words", function() {
+        expect(wordSort('hello there hello apples chicken wings there apples chicken wings')).to.eql({hello: 1, there: 1});
     });
 });
