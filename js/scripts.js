37380f34e8c00b785ade5bab41d8b3832b59bc90
Jonathan Lin & Kyle Giard-Chase <student@epicodus.com>
2015-04-09 10:15:24 -0700

    Pass first test to return hello when given input hello

---
+++ js/scripts.js
@@ -0,0 +1,3 @@
+var wordSort = function() {
+    return 'hello';
+}
---
+++ spec/specs.js
@@ -0,0 +1,5 @@
+describe('wordSort' ,function() {
+    it("will return one word if user inputs one word", function() {
+        expect(wordSort('hello')).to.equal('hello')
+    });
+});
